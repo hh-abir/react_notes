71c9956f0caf60f9e1c53bf7bc785a2d36cf0cf4
abir <abir_04@outlook.com>
2021-09-05 20:10:47 +0600

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import { makeStyles, Drawer, Typography, List, ListItem, ListItemIcon, ListItemText, AppBar, Toolbar, Avatar  } from '@material-ui/core'
+import { useHistory, useLocation } from 'react-router-dom'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
+
+import { format } from 'date-fns'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: '#f9f9f9',
+      width: '100%',
+      padding: theme.spacing(3),
+    },
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: '#f593ec'
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    date: {
+      flexGrow: 1
+    },
+    toolbar: theme.mixins.toolbar,
+    avatar: {
+      marginLeft: theme.spacing(2)
+    }
+  }
+})
+
+export default function Layout({ children }) {
+  const classes = useStyles()
+  const history = useHistory()
+  const location = useLocation()
+
+  const menuItems = [
+    { 
+      text: 'My Notes', 
+      icon: <SubjectOutlined color="secondary" />, 
+      path: '/' 
+    },
+    { 
+      text: 'Create Note', 
+      icon: <AddCircleOutlineOutlined color="secondary" />, 
+      path: '/create' 
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      {/* app bar */}
+      <AppBar 
+        position="fixed" 
+        className={classes.appBar}
+        elevation={0}
+        color="primary"
+      >
+        <Toolbar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), 'do MMMM Y')}
+          </Typography>
+          <Typography>Abir</Typography>
+          <Avatar className={classes.avatar} src="/ninja-me.png" />
+        </Toolbar>
+      </AppBar>
+
+      {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{ paper: classes.drawerPaper }}
+        anchor="left"
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            My Notes
+          </Typography>
+        </div>
+
+        {/* links/list section */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem 
+              button 
+              key={item.text} 
+              onClick={() => history.push(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+        
+      </Drawer>
+
+      {/* main content */}
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        { children }
+      </div>
+    </div>
+  )
+}
